be9c345ecddd34f6a528bf0be4fb4c946e5f7fa1
pranjal9424 <pranjalgupta9424@gmail.com>
2020-09-28 16:39:19 +0530

    nagarro coding question

--- nagarro/1.c
+++ nagarro/1.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
-int pri(int n);
+int prime(int n);
 int main()
 {
     int n,i;
     scanf("%d",&n);
     for(i=1;i<=n;i++){
-        if(pri(i)==1){
+        if(prime(i)==1){
             printf("%d ",i);
         }
     }
     return 0;
 }
-int pri(int n)
+int prime(int n)
 {
     int i;
     for(i=2;i<=n/2;i++)
