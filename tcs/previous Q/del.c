4613d6fdd3402fbc2ead5613dde229a869e1d50c
pranjal9424 <pranjalgupta9424@gmail.com>
2020-10-14 02:55:32 +0530

    tcs

---
+++ tcs/previous Q/del.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+int a;
+void main()
+{
+    /*float b;
+    b=(float)3/2;*/
+    int c=50,x=5;
+    while(x<=c){
+    printf("%d\n",x);
+    x=x*5;
+    }
+/*
+int n=2.5;
+do{
+    printf("%d",n*2);
+}while(n>3 && n<10);
+
+
+    //return 0;
+*/}
+
