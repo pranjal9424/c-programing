a766cd9df8627e8b4b1ab2ffd6f260ffbad0c1c8
pranjal9424 <pranjalgupta9424@gmail.com>
2020-10-16 20:53:38 +0530

    nagarro

---
+++ assigment/pattern/1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            printf("1");
+        printf("\n");
+    }
+    return 0;
+}
